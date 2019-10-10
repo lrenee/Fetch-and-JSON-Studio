7886c91e9a6fc8777bbdb92f8bcd7e266e26d16a
Renee <lrenee.mo@sbcglobal.net>
2019-10-10 14:58:52 -0500

    moving from laptop in class to Mac at home

--- script.js
+++ script.js
@@ -3,54 +3,40 @@
 window.addEventListener("load", function(){
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then (function (response) {
       response.json().then(function(json) {
-        console.log(json);
+        let jsonSort = [];
+  //  while (json.length > 0) {
+          console.log(json[indexOf(Math.max(json.hoursInSpace))]);
+        //   jsonSort.push(json.splice(json[indexOf(Math.max(json.hoursInSpace))]));
+        // }
+        console.log(jsonSort);
         const div = document.getElementById("container");
+        let outline = '';
         for (let i = 0; i < json.length; i++) {
-          let astronautName = document.getElementById("astronautName");
-          astronautName.innerHTML = `${json[i].firstName} ${json[i].lastName}`
-          console.log(astronautName);
-          let spaceHours = document.getElementById("spaceHours");
-          spaceHours.innerHTML = `Hours in space: ${json[i].hoursInSpace}`;
-          let currentStatus = document.getElementById("currentStatus");
-          currentStatus.innerHTML = `Active: ${json[i].active}`;
-          // for (let j = 0; j < json[i].skills.length; j++) {
-          //   json[i].skills.length);
-          // };
-          let skillset = document.getElementById("skillset");
-          skillset.innerHTML = `Skills: ${json[i].skills.join(', ')}`;
-          let image = document.getElementById("image");
-          image.innerHTML = `src = ${json[i].picture}`;
-            
-         };
-      });         
-
-
-
-// "id": 1,
-// "active": false,
-// "firstName": "Mae",
-// "lastName": "Jemison",
-// "skills": [
-//       "Physician", "Chemical Engineer"
-// ],
-// "hoursInSpace": 190,
-// "picture": "mae-jemison.jpg"
-
-//       <div class="astronaut">
-//         <div class="bio">
-//            <h3>Mae Jemison</h3>
-//            <ul>
-//               <li>Hours in space: 190</li>
-//               <li>Active: false</li>
-//               <li>Skills: Physician, Chemical Engineer</li>
-//            </ul>
-//         </div>
-//         <img class="avatar" src="images/mae-jemison.jpg">
-//      </div>
+        
+        box = `
+          <div class="astronaut">
+          <div class="bio">
+            <h3>${json[i].firstName} ${json[i].lastName}</h3>
+            <ul>
+              <li>Hours in space: ${json[i].hoursInSpace}</li>
+              <li>Active: ${json[i].active}</li>
+              <li>Skills: ${json[i].skills.join(', ')}</li>
+            </ul>
+          </div>
+          <img class="avatar" src=${json[i].picture}>
+          </div>`
+        
+       outline += box;   
      
-     
+       };
+     div.innerHTML += outline;  
+    });     
   });
 });
+    
+
+         
+     
 
 
 
